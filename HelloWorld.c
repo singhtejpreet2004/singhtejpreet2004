fb2aa5b1d04dc0d0b5227e034dcb37a77f787dd9
Tejpreet Singh <109855457+singhtejpreet2004@users.noreply.github.com>
2022-10-05 11:35:50 +0530

    Create HelloWorld.c
    I Have Recently Started my Coding Journey(I am Learning C Programming Language!) and I shall be Posting all the Code that I Write in this repository;
    Following is a Standard I/O Code that Prints anything inside the "_" as it is onto the Output Screen.
    In this particular Code "Hello World !" is printed onto the output screen
    
    P.S: For more please refer to the code comments.

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+//This is a Standard I/O Code -It prints A Simple Hello World ! Clause onto the output Screen
+
+#include <stdio.h>//This is a Pre-Processor Directive and Header file. (Tells the computer which function performs which function!)
+
+int main()//Main Function: Acts as the sailor of our code ship-> The code wont run without it(Initiates Compilation from this function onwards)
+  
+{//Opening of code block
+  
+printf("Hello World !");// printf function is used to print the output...and the semicolan indicates ending of line of code 
+  
+  return 0;// return 0 confirms that the code has run sucessfully and print exit code = 0, else gives a garbage value
+  
+  }//Ending of Code Block
